a76df4f40226d50f1a4edb171f07ea71bb0f34c3
Eduardo N.S.R <vndmtrx@duck.com>
2025-04-18 15:33:07 -0300

    Ajustes no teste

--- 01-saudacao/test/saudacao_test.exs
+++ 01-saudacao/test/saudacao_test.exs
@@ -2,11 +2,12 @@ defmodule SaudacaoTest do
   use ExUnit.Case, async: true
   doctest Saudacao
 
+  describe "ola/1" do
     test "retorna uma saudação personalizada" do
       assert Saudacao.ola("Dudu") == "Olá, Dudu!"
     end
 
     test "retorna corretamente com nomes diferentes" do
       assert Saudacao.ola("Leitor") == "Olá, Leitor!"
-    end
+  end
 end
