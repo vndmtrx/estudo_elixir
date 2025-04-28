5b6624f980d3c77b148ccc624f29602fcaf9d2d5
Eduardo N.S.R <vndmtrx@duck.com>
2025-04-28 11:39:30 -0300

    Ajuste de Prefixo por ser recursivo e não precisar de pilha, e por isso da função monta_ast

--- 03-patterns/shunting_yard/lib/shunting_yard/analisador/prefixo.ex
+++ 03-patterns/shunting_yard/lib/shunting_yard/analisador/prefixo.ex
@@ -10,12 +10,6 @@ defmodule ShuntingYard.Analisador.Prefixo do
   # Verifica se é operador
   defp op?(token), do: token in ["+", "-", "*", "/", "%", "^"]
 
-  # Monta a AST para número
-  defp monta_ast({:num, num}), do: {:ok, {:num, num}}
-
-  # Monta a AST para operador
-  defp monta_ast({:op, op, esq, dir}), do: {:ok, {:op, op, esq, dir}}
-
   # Valida a árvore final
   defp valida_ast({:ok, ast, []}), do: {:ok, ast}
   defp valida_ast({:ok, _ast, _resto}), do: {:error, :ast_invalido}
@@ -28,23 +22,18 @@ defmodule ShuntingYard.Analisador.Prefixo do
     |> valida_ast()
   end
 
-  # Parse principal prefixo (interno)
+  # Parse principal prefixo (recursivo)
   defp parse_tokens([]), do: {:error, :faltam_operandos}
 
   defp parse_tokens([token | resto]) do
     cond do
       num?(token) ->
-        case monta_ast({:num, token}) do
-          {:ok, ast} ->
-            {:ok, ast, resto}
-            # {:error, motivo} -> {:error, motivo}
-        end
+        {:ok, {:num, token}, resto}
 
       op?(token) ->
         with {:ok, esq, resto1} <- parse_tokens(resto),
-             {:ok, dir, resto2} <- parse_tokens(resto1),
-             {:ok, ast} <- monta_ast({:op, token, esq, dir}) do
-          {:ok, ast, resto2}
+             {:ok, dir, resto2} <- parse_tokens(resto1) do
+          {:ok, {:op, token, esq, dir}, resto2}
         else
           {:error, motivo} -> {:error, motivo}
         end
